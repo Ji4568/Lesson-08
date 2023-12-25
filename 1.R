ffbe22ceef3feb3ed28f00973ce6719172b234bf
呂佶 <ji8058882@gmail.com>
2023-12-25 14:34:30 +0800

    第一節：基礎繪圖函數簡介-1(2)

--- 1.R
+++ 1.R
@@ -4,3 +4,18 @@
 #請下載本週的範例資料 “Example data.csv”
 dat = read.csv("Example data.csv", header = TRUE)
 head(dat)
+
+#基礎繪圖函數簡介-1(2)
+#我們先從幾個簡單的統計圖開始
+#直方圖：需要使用函數「hist()」
+hist(dat[,"eGFR"])
+
+#盒鬚圖：需要使用函數「boxplot()」
+boxplot(dat[,"eGFR"])
+
+#圓餅圖：需要使用函數「pie()」以及函數「table()」
+pie(table(dat[,"Education"]))
+
+#長條圖：需要使用函數「barplot()」以及函數「table()」
+barplot(table(dat[,"Education"]))
+
